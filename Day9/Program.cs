f8a60e362cf3a9f25738b23a9e4a55538ba943d4
Matthias Wieloch <saiberion@web.de>
2017-12-09 11:57:29 +0100

    Added project for Day 9

---
+++ Day9/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader file = new StreamReader("input.txt");
+            string stream;
+
+            stream = file.ReadToEnd();
+
+            Console.ReadLine();
+        }
+    }
+}
