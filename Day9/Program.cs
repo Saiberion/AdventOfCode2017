600da6d3167d9935515a8a11cd56ad7876f2f961
Matthias Wieloch <saiberion@web.de>
2017-12-09 12:32:27 +0100

    Day 9 finished sucessfully

--- Day9/Program.cs
+++ Day9/Program.cs
@@ -13,9 +13,71 @@ namespace Day9
         {
             StreamReader file = new StreamReader("input.txt");
             string stream;
+            int currentGroupScore = 0;
+            int totalScore = 0;
+            bool openGarbage = false;
+            int garbageCharachters = 0;
 
             stream = file.ReadToEnd();
+            file.Close();
 
+            for (int i = 0; i < stream.Length; i++)
+            {
+                switch(stream[i])
+                {
+                    case '{':
+                        if (!openGarbage)
+                        {
+                            currentGroupScore++;
+                        }
+                        else
+                        {
+                            garbageCharachters++;
+                        }
+                        break;
+                    case '}':
+                        if (!openGarbage)
+                        {
+                            totalScore += currentGroupScore;
+                            currentGroupScore--;
+                        }
+                        else
+                        {
+                            garbageCharachters++;
+                        }
+                        break;
+                    case '<':
+                        if (!openGarbage)
+                        {
+                            openGarbage = true;
+                        }
+                        else
+                        {
+                            garbageCharachters++;
+                        }
+                        break;
+                    case '>':
+                        openGarbage = false;
+                        break;
+                    case '!':
+                        if (openGarbage)
+                        {
+                            i++;
+                        }
+                        break;
+                    default:
+                        if (openGarbage)
+                        {
+                            garbageCharachters++;
+                        }
+                        break;
+                }
+
+               
+            }
+
+            Console.WriteLine(string.Format("Total score: {0}", totalScore));
+            Console.WriteLine(string.Format("Garbage characters: {0}", garbageCharachters));
             Console.ReadLine();
         }
     }
