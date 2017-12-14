fd260bcc16844337ee34adbbb8747c92070d2870
Matthias Wieloch <saiberion@web.de>
2017-12-14 14:14:19 +0100

    My Day 13 solution

---
+++ Day13/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace Day13
+{
+	class FirewallLayer
+	{
+		public int Depth { get; set; }
+		private int scannerPosition;
+		private int direction;
+
+		public FirewallLayer(int depth) {
+			this.Depth = depth;
+			scannerPosition = 1;
+			direction = 1;
+		}
+
+		public void moveScanner() {
+			scannerPosition += direction;
+			if (this.scannerPosition == this.Depth) {
+				direction = -1;
+			} else if (this.scannerPosition == 1) {
+				direction = 1;
+			}
+		}
+
+		public void undoMoveScanner() {
+			if (this.scannerPosition == this.Depth) {
+				direction = 1;
+			} else if (this.scannerPosition == 1) {
+				direction = -1;
+			}
+			scannerPosition -= direction;
+		}
+
+		public void resetScanner() {
+			scannerPosition = 1;
+			direction = 1;
+		}
+
+		public bool detected() {
+			return scannerPosition == 1;
+		}
+	}
+	class Program
+	{
+		static void Main(string[] args) {
+			StreamReader file = new StreamReader("input.txt");
+			string line;
+
+			Dictionary<int, FirewallLayer> firewall = new Dictionary<int, FirewallLayer>();
+			int packetLayerPosition = -1;
+			int severity = 0;
+			int maxKeyValue = int.MinValue;
+			int startDelay = 0;
+			int delay = 0;
+
+			while ((line = file.ReadLine()) != null) {
+				string[] s = line.Split(new string[] { ": " }, StringSplitOptions.RemoveEmptyEntries);
+				int keyVal = int.Parse(s[0]);
+				firewall.Add(keyVal, new FirewallLayer(int.Parse(s[1])));
+				if (keyVal > maxKeyValue) {
+					maxKeyValue = keyVal;
+				}
+			}
+
+			file.Close();
+
+			for (int i = 0; i < (maxKeyValue + 1); i++) {
+				// Move packet to next layer
+				packetLayerPosition++;
+				// check if caught
+				if (firewall.ContainsKey(packetLayerPosition)) {
+					if (firewall[packetLayerPosition].detected()) {
+						severity += packetLayerPosition * firewall[packetLayerPosition].Depth;
+					}
+				}
+				// Move all scanners
+				foreach (FirewallLayer f in firewall.Values) {
+					f.moveScanner();
+				}
+			}
+
+			packetLayerPosition = -1;
+			// Reset all scanners
+			foreach (FirewallLayer f in firewall.Values) {
+				f.resetScanner();
+				packetLayerPosition = -1;
+			}
+
+			while (packetLayerPosition <= maxKeyValue) {
+				// Move packet to next layer after delay
+				if (delay > 0) {
+					delay--;
+				} else {
+					packetLayerPosition++;
+				}
+				// check if caught
+				if (firewall.ContainsKey(packetLayerPosition)) {
+					if (firewall[packetLayerPosition].detected()) {
+						startDelay++;
+						delay = 1;
+
+						while (packetLayerPosition > 0) {
+							// Undo scanner steps
+							foreach (FirewallLayer f in firewall.Values) {
+								f.undoMoveScanner();
+							}
+							packetLayerPosition--;
+						}
+						packetLayerPosition--;
+						continue;
+					}
+				}
+				// Move all scanners
+				foreach (FirewallLayer f in firewall.Values) {
+					f.moveScanner();
+				}
+			}
+
+			Console.WriteLine(string.Format("Severity: {0}", severity));
+			Console.WriteLine(string.Format("Safe delay: {0}", startDelay));
+			Console.ReadLine();
+		}
+	}
+}
---
+++ Day13/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Allgemeine Informationen über eine Assembly werden über die folgenden 
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die mit einer Assembly verknüpft sind.
+[assembly: AssemblyTitle("Day13")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Day13")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Durch Festlegen von ComVisible auf "false" werden die Typen in dieser Assembly unsichtbar 
+// für COM-Komponenten. Wenn Sie auf einen Typ in dieser Assembly von 
+// COM zugreifen müssen, legen Sie das ComVisible-Attribut für diesen Typ auf "true" fest.
+[assembly: ComVisible(false)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid("5da9ad10-1c2d-4c71-a623-3116c99ea26b")]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Hauptversion
+//      Nebenversion 
+//      Buildnummer
+//      Revision
+//
+// Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
+// übernehmen, indem Sie "*" eingeben:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
